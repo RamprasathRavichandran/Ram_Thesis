81b70f304f83e8f51180cd291594d1de7bcb2ce3
RamprasathRavichandran <47816256+RamprasathRavichandran@users.noreply.github.com>
2021-12-25 00:03:52 +0100

    general commit after some changes in code

---
+++ robofriends/src/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = (props) => {
+  const { name, id, email } = props;
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow">
+      <h1> {name} </h1>
+      <img src={"https://robohash.org/" + name + "?200*200"} alt="robots" />
+      <div>
+        <h2>{name} </h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ robofriends/src/robots.js
@@ -0,0 +1,31 @@
+export const robots = [{
+        id: 1,
+        name: 'Ram',
+        username: 'Ram',
+        email: 'Ram@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Henry',
+        username: 'Henry',
+        email: 'Henry@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'william',
+        username: 'william',
+        email: 'william@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'mark',
+        username: 'mark',
+        email: 'mark@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Tom',
+        username: 'Tom',
+        email: 'Tom@gmail.com'
+    }
+]
